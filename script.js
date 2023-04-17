534dca975d71b4862706a38645b2f3b1abe5b55f
Joshua Anthony Domantay <dom.josh2701@gmail.com>
2023-04-16 23:25:03 -0700

    Add new question and randomize answers

--- script.js
+++ script.js
@@ -1,1 +1,60 @@
 const maxPokemon = 1008;    // As of 4/16/2023, there are 1008 Pokemons
+const allTypes = [];
+
+function getPokemonTypes() {
+    fetch("https://pokeapi.co/api/v2/type")
+        .then((response) => response.json())
+        .then((data) => {
+            for(let i = 0; i < data.results.length; i++) {
+                allTypes[i] = data.results[i].name;
+            }
+        })
+        .catch((err) => {
+            console.log("Error getting Pokemon types", err);
+        });
+}
+
+getPokemonTypes();
+
+fetch("https://pokeapi.co/api/v2/pokemon/ditto")
+    .then((response) => response.json())
+    .then((data) => {
+        if(data.types.length == 1) {
+            let options = ["", "", ""];
+            options[Math.floor(Math.random() * 3)] = data.types[0].type.name;
+            for(let i = 0; i < options.length; i++) {
+                if(options[i] == "") {
+                    let newOption = "";
+                    do {
+                        let index = Math.floor(Math.random() * allTypes.length)
+                        newOption = allTypes[index];
+                    } while(options.includes(newOption));
+                    options[i] = newOption;
+                }
+            }
+
+            document.querySelector(".wrapperQuiz").innerHTML += `
+            <div class="question" style="margin-top: 3%;">
+                <img alt="test image"
+                    src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${data.id}.png" />
+
+                <div class="answers">
+                    <div class="answerOption1">
+                        <p>${options[0]}</p>
+                    </div>
+
+                    <div class="answerOption2">
+                        <p>${options[1]}</p>
+                    </div>
+
+                    <div class="answerOption3">
+                        <p>${options[2]}</p>
+                    </div>
+                </div>
+            </div>
+            `
+        }
+    })
+    .catch((err) => {
+        console.log("Pokemon not found", err);
+    });
